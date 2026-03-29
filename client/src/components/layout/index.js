d63a1c7606fb227367663046a274e1d2c203ebf7
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2026-03-29 18:42:47 +0530

    Frontend  working

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,4 +1,12 @@
-export { Layout } from './Layout'
-export { Navbar } from './Navbar'
-export { Sidebar } from './Sidebar'
-export { Footer } from './Footer'
+import { Layout as LayoutComponent } from './Layout.jsx'
+import { Navbar } from './Navbar.jsx'
+import { Sidebar } from './Sidebar.jsx'
+import { Footer } from './Footer.jsx'
+
+export { LayoutComponent as Layout }
+export { Navbar }
+export { Sidebar }
+export { Footer }
+
+// AuthLayout is same as Layout for now
+export const AuthLayout = LayoutComponent
