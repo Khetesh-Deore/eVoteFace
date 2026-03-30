977d630dadb72f456693af99405a14ff81206fc2
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2026-03-30 22:50:33 +0530

    Done  with new folder structure

--- client/src/components/common/index.js
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button'
-export { Input } from './Input'
-export { Card } from './Card'
-export { Alert } from './Alert'
-export { Loading } from './Loading'
--- client/src/layouts/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-export const AuthLayout = ({ children }) => {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-primary to-secondary flex items-center justify-center">
-      <div className="w-full max-w-md">
-        {children}
-      </div>
-    </div>
-  )
-}
